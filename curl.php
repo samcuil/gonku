0d0aab22802581393e23d22596e6f4cee03cc358
samcuil <57761892+samcuil@users.noreply.github.com>
2019-11-14 22:08:37 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -149,7 +149,7 @@ function verif($otp, $token)
     }
 function claim($token)
     {
-    $data = '{"promo_code":"GOFOODBOBA07"}';    
+    $data = '{"promo_code":"GOFOODSANTAI19"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
@@ -163,7 +163,7 @@ function claim($token)
     }
     function claim1($token)
     {
-    $data = '{"promo_code":"GOFOODSANTAI19"}';    
+    $data = '{"promo_code":"GOFOODBOBA09"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
